02e1e287f1d515cfe116f70932a9642adc1de07b
Patrick Bahitya <patrickbahitya10@gmail.com>
2020-11-27 14:52:58 +0300

    Update and rename more_about_emmanuel_page.php to more_about_Father_page.php

---
+++ more_about_Father_page.php
@@ -0,0 +1,40 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="bootstrap/bootstrap/js/bootstrap.js">
+    <link rel="stylesheet" href="bootstrap/bootstrap/js/bootstrap.min.js">
+    <link rel="stylesheet" href="bootstrap/bootstrap/js/npm.js">
+    <link rel="stylesheet" href="bootstrap/bootsrap/fonts/glyphicons-halflings-regular.eot">
+    <link rel="stylesheet" href="bootstrap/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="style.css">
+
+    <title>patrick site</title>
+</head>
+<body>
+<div class="container">
+
+    <p> No data to show at the moment.
+
+    </p>
+
+
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+</div>
+</body>
+
+</html>
